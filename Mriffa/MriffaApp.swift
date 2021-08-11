b4369355f9a49d8c0b3b6caba22412802b2d73f7
Sergei Volkov <volkovsa@vsa.su>
2021-08-11 20:21:07 +0300

    Initial Commit

---
+++ Mriffa/MriffaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MriffaApp.swift
+//  Mriffa
+//
+//  Created by Sergei Volkov on 11.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MriffaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
